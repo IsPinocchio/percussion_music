59af982d1534cb9ad50e15883de3bdbaa5c8654f
yangyang825 <84166052+yangyang825@users.noreply.github.com>
2021-06-03 12:25:03 +0800

    Add files via upload

---
+++ common.js
@@ -0,0 +1,19 @@
+(function flexible(window, document) {
+    ////根据浏览器自适应改变main部分宽高
+    function setMainSize() {
+        var main = document.querySelector('.main');
+        var nwidth = document.documentElement.clientWidth;
+        var nheight = document.documentElement.clientHeight;
+        main.style.width = (nwidth) + 'px';
+        main.style.height = (nheight) + 'px';
+    }
+    setMainSize();
+
+    // reset rem unit on page resize
+    window.addEventListener('resize', setMainSize)
+    window.addEventListener('pageshow', function(e) {
+        if (e.persisted) {
+            setMainSize();
+        }
+    })
+}(window, document))
